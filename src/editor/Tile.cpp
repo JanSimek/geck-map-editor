fa0e28a7118c455894613b95b2833bbfc60e0983
Jan Šimek <jsimek.cz@gmail.com>
2022-03-24 23:21:08 +0100

    hack to load roof tiles on epax.map

--- src/editor/Tile.cpp
+++ src/editor/Tile.cpp
@@ -5,11 +5,12 @@ namespace geck {
 Tile::Tile(uint16_t floor, uint16_t roof) : _floor(floor), _roof(roof) {}
 
 uint16_t Tile::getFloor() const {
-    return _floor;
+    return _floor & 0x0FFF; // 4095 limit, see Dims mapper
 }
 
+// see
 uint16_t Tile::getRoof() const {
-    return _roof;
+    return _roof & 0x0FFF; // 4095 limit, see Dims mapper; roof tile IDs on epax.map
 }
 
 }  // namespace geck
