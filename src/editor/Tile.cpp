8fab4be42705c01428bff4f89099214d592c6208
Jan Šimek <jsimek.cz@gmail.com>
2022-04-16 07:52:02 +0200

    tile limit hack from Dims Mapper breaks some object alignment

--- src/editor/Tile.cpp
+++ src/editor/Tile.cpp
@@ -5,12 +5,12 @@ namespace geck {
 Tile::Tile(uint16_t floor, uint16_t roof) : _floor(floor), _roof(roof) {}
 
 uint16_t Tile::getFloor() const {
-    return _floor & 0x0FFF; // 4095 limit, see Dims mapper
+    return _floor; // & 0x0FFF; // 4095 limit, see Dims mapper
 }
 
 // see
 uint16_t Tile::getRoof() const {
-    return _roof & 0x0FFF; // 4095 limit, see Dims mapper; roof tile IDs on epax.map
+    return _roof; // & 0x0FFF; // 4095 limit, see Dims mapper; roof tile IDs on epax.map
 }
 
 }  // namespace geck
