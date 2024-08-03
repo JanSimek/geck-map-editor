32e2fa1cc384611a75d13ab1d398c8540c476a6b
Jan Šimek <jsimek.cz@gmail.com>
2024-08-03 11:21:54 +0200

    Fix build

--- src/format/dat/Dat.cpp
+++ src/format/dat/Dat.cpp
@@ -5,12 +5,12 @@
 
 namespace geck {
 
-std::unordered_map<std::filesystem::path, std::shared_ptr<DatEntry>> const& Dat::getEntries() const {
+std::unordered_map<std::string, std::shared_ptr<DatEntry>> const& Dat::getEntries() const {
     return this->entries;
 }
 
 void Dat::addEntry(const std::filesystem::path filename, std::shared_ptr<DatEntry> entry) {
-    this->entries.emplace(filename, entry);
+    this->entries.emplace(filename.string(), entry);
 }
 
 } // namespace geck
